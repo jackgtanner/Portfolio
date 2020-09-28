edff9026373e20609127f27b7b4278de666a3364
Jack <jtdruim@hotmail.co.uk>
2020-09-28 12:14:16 +0100

    overhaul of main container design, and added some basic transitions

---
+++ java.js
@@ -0,0 +1,18 @@
+
+const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+
+
+var ls = sessionStorage.getItem('namespace.visited');
+if (ls == null) {
+    sessionStorage.setItem('namespace.visited', 1)
+    tl.fromTo('.mainContainer', {y: "150%"}, {y: "0%", duration: 2}, "+=0.5")
+    tl.fromTo('.mainContainer', {opacity: 0}, {opacity: 1, duration: 1}, "-=0.5")
+}
+
+
+
+
+tl.fromTo('.welcome div', {x: "-600px"}, {x: "0px", duration: 0.5, stagger: 0.1})
+tl.fromTo('.initalContainer', {x: "600px"}, {x: "0px", duration: 0.5,}, "-=0.5")
+tl.fromTo('.welcome div', {opacity: 0}, {opacity: 1, duration: 1}, "-=0.3")
+
