a4a3c7893d57c58e0ef376bbcc1bf7d2e30c3b83
Jack <jtdruim@hotmail.co.uk>
2020-10-05 18:41:59 +0100

    changed direction of about animations

--- about.js
+++ about.js
@@ -1,4 +1,4 @@
 const timeLine = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
-gsap.fromTo('.left2', {y: "1000px"}, {y: "0px", duration: 1})
-gsap.fromTo('.right2', {y: "-1000px"}, {y: "0px", duration: 1}, "-=0.5")
+gsap.fromTo('.left2', {y: "-1000px"}, {y: "0px", duration: 1})
+gsap.fromTo('.right2', {y: "1000px"}, {y: "0px", duration: 1}, "-=0.5")
