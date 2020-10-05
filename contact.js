5ff2415e78fa7627bedbdd893985cea743decfa7
Jack <jtdruim@hotmail.co.uk>
2020-10-05 17:04:21 +0100

    added a delay to the social links so that users on mobile can see the animations

--- contact.js
+++ contact.js
@@ -3,3 +3,9 @@ const timeLine = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
 gsap.fromTo('.contactText', {y: "1000px"}, {y: "0px", duration: 1})
 gsap.fromTo('.SocialContact', {y: "-1000px"}, {y: "0px", duration: 1}, "-=0.5")
+
+$('.socialLink').click(function(e) {
+    e.preventDefault();
+    var linkUrl = $(this).attr('href');
+    setTimeout(function(url) { window.location = url; }, 1000, linkUrl);
+  });
