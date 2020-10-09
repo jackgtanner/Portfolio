652b7569936e15aaaea4520f92379d8581ed63af
Jack <jtdruim@hotmail.co.uk>
2020-10-09 16:27:47 +0100

    updated the site with new Tic-Tac-Toe project, and updating the styling in the about page slightly

--- projects.js
+++ projects.js
@@ -91,11 +91,24 @@ function populateProject(title) {
         live.href = "https://jackgtanner.github.io/EtchASketch/";
         gitHub.href = "https://github.com/jackgtanner/EtchASketch";
 
+    } else if (screenTitle.innerText === "Tic-Tac-Toe" || screenTitle.innerText === "\nTic-Tac-Toe\n") {
+        readME.innerHTML = "<div>My version of a game within the console of Tic-Tac-Toe. This is the first substancial project in the <a href='https://www.theodinproject.com/courses/ruby-programming/lessons/tic-tac-toe'> Odin Project's learning course.</a></div><div>This is the first real project in which I've had to utilise making my own classes in an Object Orientated Language, in this case, Ruby</div><div>It's a simple game of Tic-Tac-Toe between two players.</div><div>The links below will take you to the live version and the source code</div>";
+        live.href = "https://tictactoe.jackgtanner.repl.run";
+        gitHub.href = "https://github.com/jackgtanner/tic_tac_toe";
+
     }
     
 
     return
 }
+
+
+// } else if (screenTitle.innerText === "" || screenTitle.innerText === "\n\n") {
+//     readME.innerHTML = "";
+//     live.href = "";
+//     gitHub.href = "";
+
+// }
 
 
 
