cbdddbf427714cab79f3d647cbdf2ffebcc727ed
Jack <jtdruim@hotmail.co.uk>
2020-10-14 14:10:17 +0100

    added in the Hangman game to my backend-development project list

--- projects.js
+++ projects.js
@@ -101,8 +101,12 @@ function populateProject(title) {
         live.href = "https://mastermind.jackgtanner.repl.run";
         gitHub.href = "https://github.com/jackgtanner/mastermind";
 
-    }
+    } else if (screenTitle.innerText === "Hangman" || screenTitle.innerText === "\nHangman\n") {
+        readME.innerHTML = "<div>My version of the game Hangman!</div><div>A random word is chosen from a dictionary file, minimum of 5 letters</div><div>The player is able to guess a letter up to 6 times incorrectly before it's game over</div><div>Coded entirely in Ruby</div>"
+        live.href = "https://hangman-2.jackgtanner.repl.run";
+        gitHub.href = "https://github.com/jackgtanner/hangman";
 
+    }
 
     return
 }
